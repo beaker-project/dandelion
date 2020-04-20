ff7a2f852839f20930230e12c21c79fffe4aec05
Renan Rodrigo <rebarbos@redhat.com>
2020-04-20 21:38:55 +0200

    chore(deps): add webpack
    Signed-off-by: Renan Rodrigo <rebarbos@redhat.com>

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader"
+          }
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new BundleAnalyzerPlugin()
+  ]
+};

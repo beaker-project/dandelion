8086ec5b78bf87c8b18a2d79a88a96e0957979fa
Renan Rodrigo <rebarbos@redhat.com>
2020-04-21 18:13:32 +0200

    chore(deps): add eslint and prettier
    Signed-off-by: Renan Rodrigo <rebarbos@redhat.com>

--- src/App/App.tsx
+++ src/App/App.tsx
@@ -1,9 +1,5 @@
 import React from 'react';
 
-const App: React.FC = () => {
-    return(
-        <p>React Placeholder</p>
-    )
-}
+const App: React.FC = () => <p>React Placeholder</p>;
 
 export default App;

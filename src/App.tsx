514fe4ebb3e5785429da77a8d04683276aa92098
Renan Rodrigo <rebarbos@redhat.com>
2020-05-25 04:27:59 +0200

    feat: include Patternfly
    Signed-off-by: Renan Rodrigo <rebarbos@redhat.com>

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
+import '@patternfly/react-core/dist/styles/base.css';
+import {
+  Page,
+  PageHeader,
+  PageSidebar,
+  PageSection,
+  PageSectionVariants,
+} from '@patternfly/react-core';
 
-const App: React.FC = () => <p>React Placeholder</p>;
+const App: React.FC = () => {
+  const [isNavOpen, setIsNavOpen] = useState(true);
+
+  const onNavToggle = (): void => setIsNavOpen(!isNavOpen);
+
+  const Header = (
+    <PageHeader
+      logo="Logo"
+      toolbar="Toolbar"
+      avatar=" | Avatar"
+      showNavToggle
+      isNavOpen={isNavOpen}
+      onNavToggle={onNavToggle}
+    />
+  );
+
+  const Sidebar = (
+    <PageSidebar nav="Navigation" isNavOpen={isNavOpen} theme="dark" />
+  );
+
+  return (
+    <Page header={Header} sidebar={Sidebar}>
+      <PageSection variant={PageSectionVariants.darker}>
+        Section with darker background
+      </PageSection>
+      <PageSection variant={PageSectionVariants.dark}>
+        Section with dark background
+      </PageSection>
+      <PageSection variant={PageSectionVariants.light}>
+        Section with light background
+      </PageSection>
+    </Page>
+  );
+};
 
 export default App;
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,8 @@
+const path = require('path');
+
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
 
 module.exports = {
@@ -16,12 +19,80 @@ module.exports = {
       },
       {
         test: /\.(js|jsx|ts|tsx)?$/,
+        exclude: /node_modules/,
         use: [
           {
             loader: 'ts-loader',
           }
         ]
       },
+      {
+        test: /\.css$/,
+        include: [
+          path.resolve(__dirname, 'src'),
+          path.resolve(__dirname, 'node_modules/patternfly'),
+          path.resolve(__dirname, 'node_modules/@patternfly/patternfly'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-styles/css'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/styles/base.css'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/esm/@patternfly/patternfly'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-inline-edit-extension/node_modules/@patternfly/react-styles/css')
+        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader']
+      },
+      {
+        test: /\.(svg|ttf|eot|woff|woff2)$/,
+        include: [
+          path.resolve(__dirname, 'node_modules/patternfly/dist/fonts'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/styles/assets/fonts'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/styles/assets/pficon'),
+          path.resolve(__dirname, 'node_modules/@patternfly/patternfly/assets/fonts'),
+          path.resolve(__dirname, 'node_modules/@patternfly/patternfly/assets/pficon')
+        ],
+        use: {
+          loader: 'file-loader',
+          options: {
+            limit: 5000,
+            outputPath: 'fonts',
+            name: '[name].[ext]',
+          }
+        }
+      },
+      {
+        test: /\.svg$/,
+        include: input => (
+          (input.indexOf('fonts') === -1) &&
+          (input.indexOf('pficon') === -1)
+        ),
+        use: {
+          loader: 'raw-loader',
+          options: {}
+        }
+      },
+      {
+        test: /\.(jpg|jpeg|png|gif)$/i,
+        include: [
+          path.resolve(__dirname, 'src'),
+          path.resolve(__dirname, 'node_modules/patternfly'),
+          path.resolve(__dirname, 'node_modules/@patternfly/patternfly/assets/images'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-styles/css/assets/images'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-core/dist/styles/assets/images'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-core/node_modules/@patternfly/react-styles/css/assets/images'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-table/node_modules/@patternfly/react-styles/css/assets/images'),
+          path.resolve(__dirname, 'node_modules/@patternfly/react-inline-edit-extension/node_modules/@patternfly/react-styles/css/assets/images')
+        ],
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 5000,
+              outputPath: 'images',
+              name: '[name].[ext]',
+            }
+          }
+        ]
+      }
     ]
   },
   plugins: [
@@ -29,7 +100,11 @@ module.exports = {
       template: "./src/index.html",
       filename: "./index.html"
     }),
-    new BundleAnalyzerPlugin()
+    new BundleAnalyzerPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[name].bundle.css'
+    })
   ],
   resolve: {
     extensions: ['.js', '.jsx', '.ts', '.tsx'],
